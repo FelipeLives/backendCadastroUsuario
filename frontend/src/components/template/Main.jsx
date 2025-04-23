a92c9efb320e33853b07722dac2dd21eb60cd20d
FelipeLives <felipe_lives@hotmail.com>
2025-04-23 14:11:18 -0300

    first commit

---
+++ frontend/src/components/template/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css'
+import React from 'react'
+import Header from './Header'
+
+// react.fragment é um component especial do react que permite agrupar varios elementos jsx sem adicionar uma 
+// tag extra no DOM
+export default props =>
+    <React.Fragment>
+        <Header />
+        <main className='content'>
+            conteúdo
+        </main>
+    </React.Fragment>
